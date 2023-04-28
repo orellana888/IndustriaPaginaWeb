43e2bc7322406fe374cd14f18fd68734da098d41
orellana888 <henryorellana58@gmail.com>
2023-04-28 11:00:27 -0600

    front y validacion terminado

--- condominios/js/index.js
+++ condominios/js/index.js
@@ -64,26 +64,59 @@ new FinisherHeader({
           var tensor = tf.tensor2d([[parseInt(ciudad), parseInt(m2), parseInt(hab), parseFloat(ban),parseInt(planta), parseInt(ascensor), parseInt(ext), parseInt(amb), parseInt(cent),parseInt(verde),parseInt(gim),parseInt(park),parseInt(pisc),parseInt(sec)]]);
           var prediccion = modelo.predict(tensor).dataSync();
           prediccion = Math.round(prediccion, 2)
-          document.getElementById("resultado").innerHTML = "Precio: " + prediccion + "$/mes";
+          document.getElementById("resultado").innerHTML = prediccion + " por mes.";
       }else{
           document.getElementById("resultado").innerHTML = "Intenta de nuevo en un rato...";
       }
   }
 
-    function reiniciarFormulario() {
+function reiniciarFormulario() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  });
+
+  swalWithBootstrapButtons.fire({
+    title: '¿Quiere reiniciar el calculo?',
+    text: "Esta acción restablecerá el formulario!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, reiniciar!',
+    cancelButtonText: 'No, cancelar!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
       document.getElementById("m2").value = "";
       document.getElementById("hab").value = "";
+      document.getElementById("ban").value = "";
       document.getElementById("planta").value = "";
       document.getElementById("parqueo").value = "";
       document.getElementById("ascensor").selectedIndex = 0;
       document.getElementById("ext").selectedIndex = 0;
-      document.getElementById("est").selectedIndex = 0;
+      document.getElementById("amb").selectedIndex = 0;
       document.getElementById("cent").selectedIndex = 0;
-      document.getElementById("area").selectedIndex = 0;
+      document.getElementById("verde").selectedIndex = 0;
       document.getElementById("gimnasio").selectedIndex = 0;
-      document.getElementById("picsina").selectedIndex = 0;
+      document.getElementById("piscina").selectedIndex = 0;
       document.getElementById("seguridad").selectedIndex = 0;
-      document.getElementById("mantenimiento").selectedIndex = 0;
+      document.getElementById("ciudad").selectedIndex = 0;
       document.getElementById("resultado").innerHTML = "";
+      
+      swalWithBootstrapButtons.fire(
+        'Reiniciado!',
+        'El formulario ha sido reiniciado.',
+        'success'
+      )
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado',
+        'El formulario no ha sido reiniciado.',
+        'error'
+      )
     }
+  });
+}
   
