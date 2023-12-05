6158196abbcd82d7f36850c2e43fe70b46cac862
orellana888 <aogreyes19@gmail.com>
2023-12-04 22:41:22 -0600

    ultimas modificaciones

--- condominios/js/index.js
+++ condominios/js/index.js
@@ -1,38 +1,38 @@
-new FinisherHeader({
-  "count": 100,
-  "size": {
-    "min": 2,
-    "max": 8,
-    "pulse": 0
-  },
-  "speed": {
-    "x": {
-      "min": 0,
-      "max": 0.4
-    },
-    "y": {
-      "min": 0,
-      "max": 0.6
-    }
-  },
-  "colors": {
-    "background": "#201e30",
-    "particles": [
-      "#fbfcca",
-      "#d7f3fe",
-      "#ffd0a7"
-    ]
-  },
-  "blending": "overlay",
-  "opacity": {
-    "center": 1,
-    "edge": 0
-  },
-  "skew": 0,
-  "shapes": [
-    "c"
-  ]
-});
+// new FinisherHeader({
+//   "count": 100,
+//   "size": {
+//     "min": 2,
+//     "max": 8,
+//     "pulse": 0
+//   },
+//   "speed": {
+//     "x": {
+//       "min": 0,
+//       "max": 0.4
+//     },
+//     "y": {
+//       "min": 0,
+//       "max": 0.6
+//     }
+//   },
+//   "colors": {
+//     "background": "#201e30",
+//     "particles": [
+//       "#fbfcca",
+//       "#d7f3fe",
+//       "#ffd0a7"
+//     ]
+//   },
+//   "blending": "overlay",
+//   "opacity": {
+//     "center": 1,
+//     "edge": 0
+//   },
+//   "skew": 0,
+//   "shapes": [
+//     "c"
+//   ]
+// });
   var modelo = null;
   (async() =>{
       console.log("Cargando el modelo...");
